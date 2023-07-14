419b4b5b346648b9c96e17ee8d2acd0dbd7b7596
Lokiraze <lokiraze@noshamegames.com>
2023-07-14 15:42:31 -0700

    restart

--- scripts/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REPO_ROOT=$(git rev-parse --show-toplevel)
-KUBECONFIG=:"${REPO_ROOT}"/provision/kubeconfig:~/.kube/config kubectl config view --flatten > ~/.kube/config.tmp && \
-  mv ~/.kube/config.tmp ~/.kube/config
-
-
-kubectl get nodes -o=wide
